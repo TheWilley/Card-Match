f1a59fc12568ce60a1859702b9e9aaeaa4bcf8e8
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-04-14 16:18:59 +0200

    refactor: renamed "Fotter" component to "Attribution" as it better describes its purpose

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import highscoreImage from '../assets/highscore.png';
-import Footer from './Footer';
+import Footer from './Attribution';
 
 type Props = {
   score: number;
