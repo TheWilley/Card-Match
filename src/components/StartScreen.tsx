5cca5f611787867736339a19ab5a2c4b978551c9
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-04-23 12:40:17 +0200

    style: fixed some styling issues
    * the score is no longer slightly offset on the x axis
    * The "Highscore" text is not cut off in small screens
    * The startscreen looks more centered

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -31,24 +31,26 @@ function StartScreen(props: Props) {
       style={{ height: props.gameWon ? '100vh' : '4rem' }}
     >
       <div className='m-auto'>
-        <span className='text-6xl'>{props.score}</span>
-        <span ref={scoreRef} className='scorediff text-xl'>
-          {props.scoreDiff}
-        </span>
-        <hr className='mt-2 border-2 border-dashed border-pink-300' />
-        <button
-          className='capirola text-md mb-4 mt-4 block w-full rounded-full border-2 p-3 transition-all ease-in-out hover:bg-pink-500 hover:text-white'
-          onClick={props.resetGame}
-        >
-          Play
-        </button>
-        <hr className='mb-2 border-2 border-dashed border-pink-300' />
-        <div className='relative'>
-          <span className='text-6xl'>{props.highscore}</span>
-          <img
-            src={highscoreImage}
-            className='bottom-2 absolute left-[calc(100%+20px)] w-48 max-w-none'
-          />
+        <div className='mb-12'>
+          <span className='text-6xl'>{props.score}</span>
+          <span ref={scoreRef} className='scorediff text-xl absolute mt-8 ml-1'>
+            {props.scoreDiff}
+          </span>
+          <hr className='mt-2 border-2 border-dashed border-pink-300' />
+          <button
+            className='capirola text-md mb-4 mt-4 block w-full rounded-full border-2 p-3 transition-all ease-in-out hover:bg-pink-500 hover:text-white'
+            onClick={props.resetGame}
+          >
+            Play
+          </button>
+          <hr className='mb-2 border-2 border-dashed border-pink-300' />
+          <div className='relative'>
+            <span className='text-6xl'>{props.highscore}</span>
+            <img
+              src={highscoreImage}
+              className='absolute bottom-2 left-[calc(100%+20px)] w-24 sm:w-48 max-w-none'
+            />
+          </div>
         </div>
       </div>
       <div style={{ opacity: props.gameWon ? '100%' : '0', transition: 'opacity' }}>
